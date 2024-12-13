291647b524851d5a700500821dbbc3cc0cf5d61b
Zain-ul-abideen-Developer <zaindeveloper198@gmail.com>
2024-12-13 23:17:42 +0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,38 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-zain",
+      "name": "Zain",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 760,
+      "initialViewParameters": {
+        "yaw": -2.2923820215096224,
+        "pitch": 0,
+        "fov": 1.2812361540040014
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "360",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
